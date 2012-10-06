e5b5059588ec6f49f847b290de3c04262c2c8c82
Ihor <iyuvchen@gmail.com>
2012-10-05 19:47:19 -0600

    added JqGrid, jquery JS, Servlet to handle hotel data

---
+++ src/com/hts/service/IJsonService.java
@@ -0,0 +1,11 @@
+package com.hts.service;
+
+import java.util.List;
+
+import net.sf.json.JSONObject;
+
+public interface IJsonService {
+	public static final Integer PAGESIZE=10;
+
+//	String getJson(List<?> list, String currentPage);
+}
---
+++ src/org/red5/demos/oflaDemo/JsonService.java
@@ -0,0 +1,61 @@
+package org.red5.demos.oflaDemo;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hts.exceptions.AppException;
+import com.hts.service.HotelServiceImpl;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Servlet implementation class SecurityTest
+ */
+public class JsonService extends HttpServlet {
+
+	private static final long serialVersionUID = 17227839L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		// out.write(getB());
+		// out.write(getA());
+		String entityName = (String) request.getParameter("json");
+		String oper = (String) request.getParameter("oper");
+
+		System.out.print("\noper:" + oper);
+		String id= (String) request.getParameter("id");
+
+		System.out.print("\nid:" + id);
+
+		if (entityName.equals("hotels")) {
+			try {
+				out.write(new HotelServiceImpl().getJson(
+						new HotelServiceImpl().getAll(), "0"));
+			} catch (AppException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return;
+		}
+
+	}
+
+}
+
+// http://localhost:5080/oflaDemo/JsonService
